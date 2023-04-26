706dfae0f35a85c971ddba9ad2300599dfc72a3f
Navalona137 <r9navalona@gmail.com>
2023-04-26 17:53:02 +0100

    sprint5

--- test/Emp.java
+++ test/Emp.java
@@ -1,16 +1,24 @@
 package model;
 
 import etu1884.annotation.*;
+import etu1884.obj.*;
 
 @ClassAnnotation
 public class Emp {
-    @MyAnnotation
+    @MyAnnotation(value = "emp-findAll")
     public void findAll(){
         System.out.println("findAll");
     }
     
-    @MyAnnotation
+    @MyAnnotation(value = "emp-add")
     public void add(){
         System.out.println("add");
+    }
+
+    @MyAnnotation(value = "emp-view")
+    public ModelView getView(){
+    	ModelView view = new ModelView();
+    	view.setView("hello.jsp");
+    	return view;
     }
 }
